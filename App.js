9d44fccee6bb9cdf03680d6ee2141e1b6a8c2f25
PigwidgeonDeluxe <15840839+PigwidgeonDeluxe@users.noreply.github.com>
2020-05-08 17:08:24 -0400

    PMD-0: added toggle and untoggle countdown function, and reset timer function

--- App.js
+++ App.js
@@ -1,15 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
 
 export default class App extends React.Component {
+
+  state = {
+    increment: 1000,
+    setTime: 100,
+    time: 100,
+    runTimer: false,
+    repeat: false,
+  }
+
+  interval = setInterval(() => {
+    if (this.state.runTimer) {
+      console.log("decreasing time");
+      this.setState(prevState => ({time: [prevState.time - 1]}));
+    }
+  }, this.state.increment)
+
+  toggleTimer = () => {
+    this.setState(prevState => ({runTimer: !prevState.runTimer}));
+  }
+
+  resetTimer = () => {
+    this.setState({time: this.state.setTime, runTimer: false});
+  }
+
   render() {
+    
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>{this.state.time}</Text>
+        <Button title="Toggle Timer" onPress={this.toggleTimer} />
+        <Button title="Reset Timer" onPress={this.resetTimer} />
       </View>
     );
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Project for Harvard's CS50M Mobile App Development with React Native course.
 Base code downloaded from https://docs.cs50.net/mobile/2020/x/projects/1/project1.html
 
 ## Project Goals
+- create a pomodoro timer that aids the user in using the [Pomodoro Technique](https://en.wikipedia.org/wiki/Pomodoro_Technique)
 - get familiar with Javascript 6 and React Native
 - get familiar with mobile app development workflow
 
